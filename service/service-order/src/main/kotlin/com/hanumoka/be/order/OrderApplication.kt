913c5648b0e1f0b65e647f0b4d72091c8b81c30b
hanumoka <amagramer@gmail.com>
2026-09-07 13:02:57 +0900

    대충 정리

--- service/service-order/src/main/kotlin/com/hanumoka/be/order/OrderApplication.kt
+++ service/service-order/src/main/kotlin/com/hanumoka/be/order/OrderApplication.kt
@@ -1,11 +1,11 @@
-package com.hanumoka.be.order
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class OrderApplication
-
-fun main(args: Array<String>) {
-        runApplication<OrderApplication>(*args)
-}
+//package com.hanumoka.be.order
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
+//
+//@SpringBootApplication
+//class OrderApplication
+//
+//fun main(args: Array<String>) {
+//        runApplication<OrderApplication>(*args)
+//}
